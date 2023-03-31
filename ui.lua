38aa65e2e6e86c8a61891f6615a1b31148e11680
kkeyy <kkeyy-hash@protonmail.com>
2023-03-31 19:53:05 +0200

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -25,9 +25,9 @@ local library = {
     hovers = {},
     Relations = {},
     folders = {
-        main = "misunion",
-        assets = "misunion/assets",
-        configs = "misunion/configs"
+        main = "nordhook",
+        assets = "nordhook/assets",
+        configs = "nordhook/configs"
     },
     shared = {
         initialized = false,
@@ -440,7 +440,7 @@ do
         --
         theme.accent = accent
         --
-        local window = {pages = {}, loader = true, isVisible = false, pageammount = pageammount, callback = callback, wminfo = "misunion | UID : %u | Ping : %s | FPS : %u", currentPage = nil, fading = false, dragging = false, drag = Vector2.new(0,0), currentContent = {frame = nil, dropdown = nil, multibox = nil, colorpicker = nil, keybind = nil, textbox = nil}}
+        local window = {pages = {}, loader = true, isVisible = false, pageammount = pageammount, callback = callback, wminfo = "nordhook | UID : %u | Ping : %s | FPS : %u", currentPage = nil, fading = false, dragging = false, drag = Vector2.new(0,0), currentContent = {frame = nil, dropdown = nil, multibox = nil, colorpicker = nil, keybind = nil, textbox = nil}}
         --
         local main_frame = utility:Create("Frame", {Vector2.new(0,0)}, {
             Size = utility:Size(0, size.X, 0, size.Y),
@@ -906,7 +906,7 @@ do
         isVisible = false,
         callback = callback, 
         uibind = library.shared.allah, 
-        wminfo = "misunion | UID : %u | Ping : %s | FPS : %u", 
+        wminfo = "nordhook | UID : %u | Ping : %s | FPS : %u", 
         currentPage = nil, 
         fading = false, 
         dragging = false, 
